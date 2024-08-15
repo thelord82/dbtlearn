f7dee5994c6d750cf5cceed1e460d606dda84570
Marc-Andre Lord <mandrelord@gmail.com>
2024-08-15 11:44:39 -0400

    Up to 50th video

---
+++ models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        materialized = 'view'
+    )
+}}
+WITH src_hosts AS (
+    SELECT * FROM {{ ref('src__hosts') }}
+)
+SELECT
+    host_id,
+    NVL(host_name, 'Anonymous') AS host_name,
+    is_superhost,
+    created_at,
+    updated_at
+FROM
+    src_hosts
